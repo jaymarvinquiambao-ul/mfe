bd13f9612966893d08af3e9b23e53e4133791162
Jay Marvin Quiambao <83590@global.ul.com>
2025-11-26 15:54:57 +0800

    Auth Implementation

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -2,20 +2,24 @@ import React from 'react';
 import { Switch, Route, Router } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
-import Landing from './components/Landing';
-import Pricing from './components/Pricing';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
 
 const generateCLassName = createGenerateClassName({
-    productionPrefix: 'ma',
+    productionPrefix: 'au',
 });
 
-export default ({ history }) => {
+export default ({ history, onSignIn }) => {
     return <div>
         <StylesProvider generateClassName={generateCLassName}>
             <Router history={history}>
                 <Switch>
-                    <Route exact path="/pricing" component={Pricing} />
-                    <Route path="/" component={Landing} />
+                    <Route exact path="/auth/signin">
+                        <SignIn onSignIn={onSignIn} />
+                    </Route>
+                    <Route exact path="/auth/signup">
+                        <SignUp onSignIn={onSignIn} />
+                    </Route>
                 </Switch>
             </Router>
         </StylesProvider>
